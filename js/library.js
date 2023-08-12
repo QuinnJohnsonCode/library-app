113676d1bba8e624e404f3c9cdda94a1091809a5
Quinn Johnson <63220163+QuinnJohnsonCode@users.noreply.github.com>
2023-08-11 22:38:39 -0400

    Add properties for Book object constructor

---
+++ js/library.js
@@ -0,0 +1,13 @@
+let myLibrary = [];
+
+function Book(title, author, numberOfPages, hasRead, releaseDate) {
+    this.title = title;
+    this.author = author;
+    this.numberOfPages = numberOfPages;
+    this.hasRead = hasRead;
+    this.releaseDate = releaseDate;
+}
+
+function addBookToLibrary() {
+
+}
