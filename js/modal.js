5c457575e8222a22947d80fae7fc5b7d070a4a09
Quinn Johnson <63220163+QuinnJohnsonCode@users.noreply.github.com>
2023-08-13 22:58:41 -0400

    Add modal form

---
+++ js/modal.js
@@ -0,0 +1,12 @@
+const modal = document.getElementById("formModal");
+const openButton = document.getElementById("openModal");
+
+window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+});
+
+openButton.addEventListener("click", () => {
+    modal.style.display = "block";
+});
