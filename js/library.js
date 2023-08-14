daa20e1d5e15ea80223e4c244508ab6762ad9a38
Quinn Johnson <63220163+QuinnJohnsonCode@users.noreply.github.com>
2023-08-13 23:25:15 -0400

    Extract data from form

--- js/library.js
+++ js/library.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+const form = document.getElementById("modal-form");
 
 function Book(title, author, numberOfPages, hasRead, releaseDate) {
     this.title = title;
@@ -8,6 +9,21 @@ function Book(title, author, numberOfPages, hasRead, releaseDate) {
     this.releaseDate = releaseDate;
 }
 
-function addBookToLibrary() {
+function displayLibrary() {
 
 }
+
+function addBookToLibrary(title, author, numberOfPages, hasRead, releaseDate) {
+    myLibrary.push(new Book(title, author, numberOfPages, hasRead, releaseDate));
+}
+
+// Form Submission
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pageCount = document.getElementById("page-count").value;
+    const publishDate = document.getElementById("publish-date").value;
+    const read = document.getElementById("has-read").value;
+});
--- js/modal.js
+++ js/modal.js
@@ -10,3 +10,7 @@ window.addEventListener("click", (event) => {
 openButton.addEventListener("click", () => {
     modal.style.display = "block";
 });
+
+form.addEventListener("submit", () => {
+    modal.style.display = "none";
+})
