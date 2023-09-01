1c8445ae9f09c49a6179f937539daae230e6d055
Quinn Johnson <63220163+QuinnJohnsonCode@users.noreply.github.com>
2023-08-31 20:05:08 -0400

    Refactor Book object constructor to class

--- js/library.js
+++ js/library.js
@@ -2,12 +2,30 @@ let myLibrary = [];
 const form = document.getElementById("modal-form");
 const cardGrid = document.getElementById("card-grid");
 
+/*
 function Book(title, author, numberOfPages, hasRead, releaseDate) {
     this.title = title;
     this.author = author;
     this.numberOfPages = numberOfPages;
     this.hasRead = hasRead;
     this.releaseDate = releaseDate;
+}
+*/
+
+class Book {
+    title;
+    author;
+    numberOfPages;
+    hasRead;
+    releaseDate;
+    
+    constructor(title, author, numberOfPages, hasRead, releaseDate) {
+        this.title = title;
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+        this.hasRead = hasRead;
+        this.releaseDate = releaseDate;
+    }
 }
 
 function displayLibrary() {
